5a440fac79ba2a9fde6431dec7e578f87d04b6e1
abhijith-sys <abhijithani007@gmail.com>
2024-04-08 19:48:19 +0530

    ExpoAgora

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef, useState, useEffect} from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {PermissionsAndroid, Platform} from 'react-native';
+import {
+ClientRoleType,
+createAgoraRtcEngine,
+IRtcEngine,
+ChannelProfileType,
+} from 'react-native-agora';
+import AgoraComponent from './AgoraComponent';
 
+const appId = '<--Insert app ID here-->';
+const channelName = '<--Insert channel name here-->';
+const token = '<--Insert authentication token here-->';
+const uid = 0;
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.main}>
+     <AgoraComponent/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  button: {
+      paddingHorizontal: 25,
+      paddingVertical: 4,
+      fontWeight: 'bold',
+      color: '#ffffff',
+      backgroundColor: '#0055cc',
+      margin: 5,
   },
+  main: {flex: 1, alignItems: 'center'},
+  scroll: {flex: 1, backgroundColor: '#ddeeff', width: '100%'},
+  scrollContainer: {alignItems: 'center'},
+  videoView: {width: '90%', height: 200},
+  btnContainer: {flexDirection: 'row', justifyContent: 'center'},
+  head: {fontSize: 20},
 });
